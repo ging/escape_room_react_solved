7aae4465600393b50ea721b7c473fc372e2e6b12
Enrique <enrique.barra@upm.es>
2025-10-16 09:53:24 +0200

    2025 version

--- extras/puzzle/app/config/app_config.js
+++ extras/puzzle/app/config/app_config.js
@@ -43,7 +43,7 @@ let GLOBAL_CONFIG_DEVELOPMENT = {
 
   // Escapp configuraciones
   escapp:{
-    endpoint:"https://escape.dit.upm.es/api/escapeRooms/367",
+    endpoint:"https://escape.dit.upm.es/api/escapeRooms/381",
     localStorageKey:"ESCAPP_Puzzle",
     imagesPath:"assets/images/",
     I18n:{
